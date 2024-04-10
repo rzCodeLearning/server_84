449232b411a2ea546a52593988302e7e0d1f82f5
runze <rz_code_learning@outlook.com>
2024-04-10 13:44:49 +0800

    commit the crontab file

--- daily_download1.py
+++ daily_download1.py
@@ -1,11 +1,16 @@
 import datetime as dt
 import os
 
+print('--daily download1 begins--')
+
 with open('/home/zrz/rqdata_daily//trading_dates.txt', 'r') as file:
     trading_dates = file.readlines()
 trading_dates = [x[0:8] for x in trading_dates]
 
 today = dt.datetime.today().strftime("%Y%m%d")
+print(f'today is {today}')
 
 if today in trading_dates:
     os.system(f'/home/zrz/.conda/envs/rqsdk/bin/python /home/zrz/rqdata_daily/download_from_rqdata1.py {today}')
+
+print('--daily download1 ends--')
--- daily_download2_risk.py
+++ daily_download2_risk.py
@@ -1,12 +1,17 @@
 import datetime as dt
 import os
 
+print('--daily download2 begins--')
+
 with open('/home/zrz/rqdata_daily//trading_dates.txt', 'r') as file:
     trading_dates = file.readlines()
 trading_dates = [x[0:8] for x in trading_dates]
 
 today = dt.datetime.today().strftime("%Y%m%d")
+print(f'today is {today}')
 
 if today in trading_dates:
     yesterday = trading_dates[trading_dates.index(today) - 1]
     os.system(f'/home/zrz/.conda/envs/rqsdk/bin/python /home/zrz/rqdata_daily/download_from_rqdata2.py {yesterday}')
+
+print('--daily download2 ends--')
